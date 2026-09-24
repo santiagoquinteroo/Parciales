0bbed5e18603686b7963cd18cd54234fab4da57d
Juan1504-bit <juanj.garciag1@uqvirtual.edu.co>
2026-09-23 21:33:05 -0500

    Update Hotel.java

--- Parcial_1/src/main/java/co/edu/uniquindio/poo/model/Hotel.java
+++ Parcial_1/src/main/java/co/edu/uniquindio/poo/model/Hotel.java
@@ -1,6 +1,58 @@
 package co.edu.uniquindio.poo.model;
 
 public class Hotel {
-    
+    private String nombre;
+    private String nit;
+    private String direccion;
+    private String telefono;
 
+
+    public Hotel(String nombre, String nit, String direccion, String telefono) {
+        this.nombre = nombre;
+        this.nit = nit;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "nombre='" + nombre + '\'' +
+                ", nit='" + nit + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
+    }
 }
