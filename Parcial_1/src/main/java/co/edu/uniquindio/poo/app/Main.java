b42f63bb27725e227a497e6f367c65d1286cb15d
santiagoquinteroo <santiago.quinteroo@uqvirtual.edu.co>
2026-09-23 21:54:29 -0500

    Creaciondelmenu.java
    se creó la interfaz

--- Parcial_1/src/main/java/co/edu/uniquindio/poo/app/Main.java
+++ Parcial_1/src/main/java/co/edu/uniquindio/poo/app/Main.java
@@ -1,6 +1,30 @@
 package co.edu.uniquindio.poo.app;
 
+import javax.swing.JOptionPane;
+
 public class Main {
-    
+    static void main(){
+        JOptionPane.showMessageDialog(null, "Bienvenid@ al hotel StayPlus");
+
+        int opcion;
+
+        do {
+            opcion=Integer.valueOf(JOptionPane.showInputDialog(null,"------MENÚ------ \n"+
+                                                                "1. Consultar huesped \n"+
+                                                                "2. Control de disponibilidad de habitaciones \n" +
+                                                                "3. Matriz de ocupación del Hotel \n"+
+                                                                "4. Número especial de reserva \n"+
+                                                                "0. Ingresos del Hotel"));
+
+
+            switch (opcion){
+                case 1:
+                    break;
+
+                default:JOptionPane.showMessageDialog(null,"Opción no encontrada o invalida, intente nuevamente");
+            }
+
+        }while(opcion!=0);
+    }
 
 }
