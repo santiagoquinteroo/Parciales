1808a737d2d9757d6977b2110ac984ad6b11fa3f
santiagoquinteroo <santiago.quinteroo@uqvirtual.edu.co>
2026-09-23 21:58:23 -0500

    Update Hotel.java

--- Parcial_1/src/main/java/co/edu/uniquindio/poo/model/Hotel.java
+++ Parcial_1/src/main/java/co/edu/uniquindio/poo/model/Hotel.java
@@ -1,49 +1,71 @@
 package co.edu.uniquindio.poo.model;
 
+import java.util.ArrayList;
+
 public class Hotel {
     private String nombre;
     private String nit;
     private String direccion;
     private String telefono;
 
+    private ArrayList<Huesped>listaHuespedes;
+    private ArrayList<Habitacion>listaHabitaciones;
 
-    public Hotel(String nombre, String nit, String direccion, String telefono) {
+    public Hotel(String nombre, String nit, String direccion, String telefono, ArrayList<Huesped> listaHuespedes, ArrayList<Habitacion> listaHabitaciones) {
         this.nombre = nombre;
         this.nit = nit;
         this.direccion = direccion;
         this.telefono = telefono;
+        this.listaHuespedes = new ArrayList<>();
+        this.listaHabitaciones = new ArrayList<>();
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public String getNit() {
-        return nit;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getNit() {
+        return nit;
     }
 
     public void setNit(String nit) {
         this.nit = nit;
     }
 
+    public String getDireccion() {
+        return direccion;
+    }
+
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+    public String getTelefono() {
+        return telefono;
+    }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
+    }
+
+    public ArrayList<Huesped> getListaHuespedes() {
+        return listaHuespedes;
+    }
+
+    public void setListaHuespedes(ArrayList<Huesped> listaHuespedes) {
+        this.listaHuespedes = listaHuespedes;
+    }
+
+    public ArrayList<Habitacion> getListaHabitaciones() {
+        return listaHabitaciones;
+    }
+
+    public void setListaHabitaciones(ArrayList<Habitacion> listaHabitaciones) {
+        this.listaHabitaciones = listaHabitaciones;
     }
 
     @Override
@@ -53,7 +75,8 @@ public class Hotel {
                 ", nit='" + nit + '\'' +
                 ", direccion='" + direccion + '\'' +
                 ", telefono='" + telefono + '\'' +
+                ", listaHuespedes=" + listaHuespedes +
+                ", listaHabitaciones=" + listaHabitaciones +
                 '}';
     }
-
 }
